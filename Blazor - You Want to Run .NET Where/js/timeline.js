560c7f391f8cf0a739ef2bdb985f08879313296c
Jamie Taylor <jamie@gaprogman.com>
2019-02-25 08:35:05 +0000

    Fixed scrolling issue on timeline

--- Blazor - You Want to Run .NET Where/js/timeline.js
+++ Blazor - You Want to Run .NET Where/js/timeline.js
@@ -39,11 +39,12 @@
   // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
   function isElementInViewport(el) {
     const rect = el.getBoundingClientRect();
+    const rightBound = document.getElementById('timeline').offsetWidth;
     return (
       rect.top >= 0 &&
       rect.left >= 0 &&
       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      rect.right + el.offsetWidth <= rightBound
     );
   }
 
